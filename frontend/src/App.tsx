b0552e0f25fff01a1b1b97bfcd58a61537e3b0dd
Nodirbek Ergashev <nodirbek.ergashev@zoftify.com>
2023-10-31 19:19:44 +0500

    feat: Create empty react js app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/nodirshox/habit-maker">
+        habit-maker.app
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Habit Maker
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
