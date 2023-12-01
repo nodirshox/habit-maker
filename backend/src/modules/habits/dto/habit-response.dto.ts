b46ab04a549cccaf5463db3eb3c07fa97a35dc7c
Abdulla Shoymardonov <47627096+AbdullaShoymardonov1998@users.noreply.github.com>
2023-12-01 10:07:25 +0500

    feat: habit color and repetitions  (#8)
    * feat:habit color and repeatitions
    
    * fix: naming repetitions

--- backend/src/modules/habits/dto/habit-response.dto.ts
+++ backend/src/modules/habits/dto/habit-response.dto.ts
@@ -10,6 +10,9 @@ export class HabitResponseDto {
   @ApiProperty({ description: 'Habit title', example: 'No Sugar' })
   title: string
 
+  @ApiProperty({ description: 'Habit color', example: '#ddd' })
+  color: string
+
   @ApiProperty({ description: 'Habit created time', example: new Date() })
   createdAt: Date
 
