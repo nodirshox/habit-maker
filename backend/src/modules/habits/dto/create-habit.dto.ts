154253f8d14c2dd31e2c197191a7d7185d432f9b
Abdulla Shoymardonov <abdulla1998uz@gmail.com>
2023-12-06 15:44:42 +0500

    fix:updated repetitions

--- backend/src/modules/habits/dto/create-habit.dto.ts
+++ backend/src/modules/habits/dto/create-habit.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Weekdays } from '@prisma/client'
 import {
-  ArrayMaxSize,
-  ArrayMinSize,
   IsArray,
-  IsEnum,
+  IsBoolean,
   IsNotEmpty,
+  IsNumber,
+  IsOptional,
   IsString,
+  Max,
+  Min,
 } from 'class-validator'
 
 export class CreateHabitDto {
@@ -28,17 +29,108 @@ export class CreateHabitDto {
   })
   color: string
 
+  @IsOptional()
   @IsArray()
-  @ArrayMinSize(1)
-  @ArrayMaxSize(7)
-  @IsEnum(Weekdays, { each: true })
   @ApiProperty({
-    description: 'Repetitions',
+    description: 'WeekDays',
     required: true,
-    type: [String],
-    example: [Weekdays.MONDAY],
+    // type: [{ day: String, isSelected: Boolean }],
+    example: [
+      { day: 'Monday', isSelected: false },
+      { day: 'Tuesday', isSelected: false },
+      { day: 'Wednesday', isSelected: false },
+    ],
   })
-  repetitions: Weekdays[]
+  weekdays: { day: string; isSelected: boolean }[]
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty({
+    description: 'Number of Days',
+    required: true,
+    example: 7,
+  })
+  numberOfDays: number
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Notification Time',
+    required: true,
+    example: '12:30',
+  })
+  notifyTime: string
+
+  @IsNotEmpty()
+  @IsBoolean()
+  @ApiProperty({
+    description: 'Show Notification',
+    required: true,
+    example: true,
+  })
+  showNotification: boolean
 }
 
-export class UpdateHabitDto extends CreateHabitDto {}
+export class UpdateHabitDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Name',
+    required: true,
+    example: 'No sugar',
+  })
+  title: string
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Color',
+    required: true,
+    example: '#ddd',
+  })
+  color: string
+
+  @IsArray()
+  @IsNotEmpty()
+  type: () => JSON
+  @ApiProperty({
+    description: 'WeekDays',
+    required: true,
+    // type: [{ day: String, isSelected: Boolean }],
+    example: [
+      { day: 'Monday', isSelected: false },
+      { day: 'Tuesday', isSelected: false },
+      { day: 'Wednesday', isSelected: false },
+    ],
+  })
+  weekdays: { day: string; isSelected: boolean }[]
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
+  @Max(7)
+  @ApiProperty({
+    description: 'Number of Days',
+    required: true,
+    example: 7,
+  })
+  numberOfDays: number
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Notification Time',
+    required: true,
+    example: '12:30',
+  })
+  notifyTime: string
+
+  @IsNotEmpty()
+  @IsBoolean()
+  @ApiProperty({
+    description: 'Show Notification',
+    required: true,
+    example: true,
+  })
+  showNotification: boolean
+}
